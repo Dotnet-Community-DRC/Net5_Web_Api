36bcce372e9e4bfeecd73436596695bdfcb8b746
$udiSimba <sudisimbadav@gmail.com>
2020-11-24 12:22:58 +0200

    Add project files.

---
+++ NetCoreWeb.Api/Models/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace NetCoreWeb.Api.Models
+{
+    public class AppDbContext : IdentityDbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
